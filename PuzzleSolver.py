008daefdda2f8dfb7e9911fe2395d78e10213265
FlynnMcG <fmcge001@ucr.edu>
2016-11-03 16:30:56 -0700

    Started main loop

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -1,10 +1,17 @@
 import math
 import numpy
 
+
 startingConfig=numpy.array(([1,2,3,4],[5,6,7,8],[9,10,11,12],[15,14,13,0]), dtype=int)
 goalConfig=numpy.array(([1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]), dtype=int)
-M=4
+queue=[]
 
+iterations=0
+maxdepth=0
+tuple=(startingConfig,0)
+queue.append(tuple)
+M=4
+current=0
 
 
 
@@ -31,6 +38,23 @@ def ManhattanD(currentArray):
                 difference+=abs(goalI-i)+abs(goalJ-j)
     return difference
 
+while True:
+    if queue.__sizeof__()>maxdepth:
+        maxdepth=queue.__sizeof__()
+    queue.sort(key=lambda tup: tup[1])
+    current=queue.pop()[0]
+    if current==goalConfig:
+        break
+    zI=numpy.where(startingConfig==0)[0][0]
+    zJ=numpy.where(startingConfig==0)[1][0]
+    if(zI>0):
+        temp=current
+    if(zI<M-1):
+
+    if(zJ>0):
+
+    if(zJ<M-1):
+
 
 
 
@@ -38,4 +62,4 @@ def ManhattanD(currentArray):
 #print startingConfig
 #print goalConfig
 #print MisplacedTileD(startingConfig)
-print ManhattanD(startingConfig)
+#print ManhattanD(startingConfig)
