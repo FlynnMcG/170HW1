a2504738fa7efd19f714aa66465eef155f286110
FlynnMcG <fmcge001@ucr.edu>
2016-11-03 20:08:37 -0700

    Ignores previously visited states if depth isn't improved

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -2,7 +2,7 @@ import math
 import numpy
 
 
-startingConfig=numpy.array(([0,1,3],[4,2,5],[7,8,6]), dtype=int)
+startingConfig=numpy.array(([1,2,3],[4,8,0],[7,6,5]), dtype=int)
 goalConfig=numpy.array(([1,2,3],[4,5,6],[7,8,0]), dtype=int)
 queue=[]
 
@@ -12,6 +12,7 @@ tuple=(startingConfig,0,0,hash(startingConfig.tostring()))
 queue.append(tuple)
 M=3
 current=0
+deadQueue=[]
 
 
 def MisplacedTileD(currentArray):
@@ -57,6 +58,8 @@ while True:
         prev = i[3]
     queue.sort(key=lambda tup: -tup[1])
     current=queue.pop()
+    dead=(current[2],current[3])
+    deadQueue.append(dead)
     # print current[1]
     depth=current[2]+1
 
@@ -70,14 +73,31 @@ while True:
         temp=numpy.copy(current)
         temp[zI][zJ],temp[zI-1][zJ]=temp[zI-1][zJ],temp[zI][zJ]
         temp=(temp,ManhattanD(temp)+depth,depth, hash(temp.tostring()))
-        queue.append(temp)
+        checker=[i for i, v in enumerate(deadQueue) if v[1] == temp[3]]
+        if len(checker)>0:
+            if depth>=deadQueue[checker[0]][0]:
+                depth=depth
+            else:
+                deadQueue[checker][0]=depth
+                queue.append(temp)
+        else:
+            queue.append(temp)
+
         # print "up"
         # print temp
     if(zI<M-1):
         temp = numpy.copy(current)
         temp[zI][zJ], temp[zI + 1][zJ] = temp[zI + 1][zJ], temp[zI][zJ]
         temp = (temp, ManhattanD(temp) + depth, depth, hash(temp.tostring()))
-        queue.append(temp)
+        checker = [i for i, v in enumerate(deadQueue) if v[1] == temp[3]]
+        if len(checker) > 0:
+            if depth >= deadQueue[checker[0]][0]:
+                depth = depth
+            else:
+                deadQueue[checker][0] = depth
+                queue.append(temp)
+        else:
+            queue.append(temp)
         # print "down"
         # print temp
     if(zJ>0):
@@ -85,16 +105,33 @@ while True:
         temp = numpy.copy(current)
         temp[zI][zJ], temp[zI][zJ-1] = temp[zI][zJ-1], temp[zI][zJ]
         temp = (temp, ManhattanD(temp) + depth, depth, hash(temp.tostring()))
-        queue.append(temp)
+        checker = [i for i, v in enumerate(deadQueue) if v[1] == temp[3]]
+        if len(checker) > 0:
+            if depth >= deadQueue[checker[0]][0]:
+                depth = depth
+            else:
+                deadQueue[checker][0] = depth
+                queue.append(temp)
+        else:
+            queue.append(temp)
         # print "left"
         # print temp
     if(zJ<M-1):
         temp = numpy.copy(current)
         temp[zI][zJ], temp[zI][zJ+1] = temp[zI][zJ+1], temp[zI][zJ]
         temp = (temp, ManhattanD(temp) + depth, depth, hash(temp.tostring()))
-        queue.append(temp)
+        checker = [i for i, v in enumerate(deadQueue) if v[1] == temp[3]]
+        if len(checker) > 0:
+            if depth >= deadQueue[checker[0]][0]:
+                depth = depth
+            else:
+                deadQueue[checker][0] = depth
+                queue.append(temp)
+        else:
+            queue.append(temp)
         # print "right"
         # print temp
+
 
 
 print maxsize
